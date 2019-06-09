02dfaf052bc1a98cbd3342fc81f5562922ac1b37
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-09 21:28:02 +0200

    [dev] (feat): new database schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -9,21 +9,30 @@ const knex = require('knex')({
     user: 'service_auth',
     password: process.env.AUTH_DATABASE_PASSWORD
   },
-  searchPath: 'sc_auth'
+  searchPath: 'auth'
 })
+
+const isValidUUID = uuid =>
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+    uuid
+  )
 
 export default {
   Query: {
     hello: () => 'authentication service says hello',
-    user: async (root, { token: sessionToken }, { isService }) => {
+    user: async (root, { sessionToken }, { isService }) => {
       if (!isService) throw new ForbiddenError('UNAUTHORIZED')
+      if (!isValidUUID(sessionToken)) throw new UserInputError('INVALID_UUID')
       const session = (await knex('session')
-        .select('uid')
+        .select('id')
         .where({ token: sessionToken }))[0]
       if (!session) throw new UserInputError('NOT_LOGGED_IN')
-      return (await knex('user').where({ uid: session.uid }))[0]
+      return (await knex('user').where({ id: session.id }))[0]
     }
-  }
+  },
 
-  // Mutation: {}
+  Mutation: {
+    login: async (root, { email, password }) => {},
+    logout: async (root, { sessionToken }) => {}
+  }
 }
